d24a9b2cf38346096782f2fe15db9f11c7fcee83
TanakaMhuka <mhukatanaka99@gmail.com>
2023-02-17 03:17:36 -0800

    all

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * conditionals
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -20,3 +22,4 @@ int main(void)
 
 	return (0);
 }
+
