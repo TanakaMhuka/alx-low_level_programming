2a0a38765008ee3f92787e108de169348bb732af
TanakaMhuka <mhukatanaka99@gmail.com>
2023-02-17 06:59:23 -0800

    8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,22 +1,25 @@
 /*
- * File: 6-print_numberz.c
+ * File: 8-print_base16.c
  * Auth: Tanaka
  */
 
 #include <stdio.h>
 
 /**
- * main - Prints all single digit numbers of base 10 starting from 0,
- *        only using putchar and without char variables.
+ * main - Prints all the numbers of base 16 in lowercase.
  *
  * Return: Always 0.
  */
 int main(void)
 {
 	int num;
+	char letter;
 
 	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
+
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
 
 	putchar('\n');
 
