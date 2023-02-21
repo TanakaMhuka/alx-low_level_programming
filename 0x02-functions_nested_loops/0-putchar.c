be5ce753bf8bbb1dbcf044de9af169ce840d8cd4
TanakaMhuka <mhukatanaka99@gmail.com>
2023-02-21 03:13:52 -0800

    zero

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
-int main(void)
-{
-	char text[]="_putchar\n";
-	for(int	i=0;text[i]!='\n';i++)
-	{
-		putchar(text[i]);
-	}
-	return 0;	
+int main() {
+    putchar('_');
+    putchar('p');
+    putchar('u');
+    putchar('t');
+    putchar('c');
+    putchar('h');
+    putchar('a');
+    putchar('r');
+    putchar('\n');
+    return 0;
 }
+
